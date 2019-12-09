c5ac8776f97217621142b1c82d6ac0a7ceffe52a
glebkalachev <megas781@gmail.com>
2019-12-09 16:20:37 +0300

    all made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab11';
+  sensors: Sensor[] = [];
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.sensors.push(Sensor.initRandomized());
+    }
+  }
+
+
+
+
+}
+
+class Sensor {
+  public id: number;
+  public active: boolean;
+
+  constructor() {
+  }
+  static init(id, active) {
+    const newObj = new Sensor();
+    newObj.id = id;
+    newObj.active = active;
+  }
+  static initRandomized(): Sensor {
+    const newObj = new Sensor();
+    newObj.id = Math.floor(Math.random() * 100);
+    newObj.active = Boolean(Math.round(Math.random()));
+    return newObj;
+  }
+
 }
