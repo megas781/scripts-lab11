e7f3eb0af2002e6a820545b2d609423f674628f3
glebkalachev <megas781@gmail.com>
2019-12-10 11:25:00 +0300

    adding function implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LifecycleHooks} from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,46 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lab11';
   sensors: Sensor[] = [];
+  modalView: HTMLElement;
+
+  newSensorId: string;
+
+
+
+  isNewSensorActive: boolean;
+
   constructor() {
     for (let i = 0; i < 10; i++) {
       this.sensors.push(Sensor.initRandomized());
     }
   }
+  ngAfterViewInit() {
+    this.modalView = document.querySelector('.add-sensor-modal');
+  }
+
   deleteSensor(index) {
     console.log(index);
     this.sensors.splice(index, 1);
   }
 
+  public showModal() {
+    this.modalView.classList.remove('hidden');
+    (this.modalView.querySelector('input[name=newSensorId]') as HTMLElement).focus();
+  }
+  public addSensorWithModal() {
+    const newSensor = Sensor.init(this.newSensorId, this.isNewSensorActive);
+    this.sensors.push(newSensor);
+    this.hideModal();
+  }
+  private hideModal() {
+    this.modalView.classList.add('hidden');
+    this.newSensorId = '';
+    this.isNewSensorActive = false;
+  }
+
 }
+
+
 
 class Sensor {
   public id: number;
@@ -30,6 +60,7 @@ class Sensor {
     const newObj = new Sensor();
     newObj.id = id;
     newObj.active = active;
+    return newObj;
   }
   static initRandomized(): Sensor {
     const newObj = new Sensor();
